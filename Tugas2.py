ed4bec08aa53586030bf3bffb04c3ef26b9f0713
dandistw10 <dandisetiawan00@gmail.com>
2021-10-28 16:49:52 +0700

    Update Tugas2.py
    Revisi

--- Tugas2.py
+++ Tugas2.py
@@ -1,6 +1,4 @@
 from selenium import webdriver
-import time
-
 
 #open chrome
 driver = webdriver.Chrome(executable_path='C:\chromedriver\chromedriver.exe')
@@ -8,61 +6,27 @@ driver = webdriver.Chrome(executable_path='C:\chromedriver\chromedriver.exe')
 #open URL
 driver.get('https://demoqa.com/webtables')
 
-#Wait time
-#time.sleep(2)
 
 #Data
 datas = [
-    ['Dandi','Setiawan','dandi@gmail.com','20','10000','IT'],
-    ['Zulfa','Dinda','dinda@gmail.com','21','20000','Finance'],
-    ['Andi','Nugroho','andi@gmail.com','22','30000','Marketing']
+    {'firstName':'Dandi','lastName':'Setiawan','userEmail':'dandi@gmail.com','age':'20','salary':'1000','department':'IT'},
+    {'firstName':'Zulfa','lastName':'Dinda','userEmail':'dinda@gmail.com','age':'21','salary':'2000','department':'Finance'},
+    {'firstName':'Andi','lastName':'Nugroho','userEmail':'andi@gmail.com','age':'22','salary':'3000','department':'Marketing'}
     ]
 
 #Iterate data
 for data in datas:
-    count = 0
 
-    button = driver.find_element_by_id('addNewRecordButton')
-    button.click()
+    driver.find_element_by_id('addNewRecordButton').click()
 
-    textfirst = driver.find_elements_by_id('firstName')
-    textlast = driver.find_elements_by_id('lastName')
-    textemail = driver.find_elements_by_id('userEmail')
-    textage = driver.find_elements_by_id('age')
-    textsalary = driver.find_elements_by_id('salary')
-    textdepartment = driver.find_elements_by_id('department')
-
-    # Iterate all input text
-    for value in textfirst:
-        value.send_keys(data[count])
-        count += 1
-        time.sleep(2)
-
-    for value in textlast:
-        value.send_keys(data[count])
-        count += 1
-        time.sleep(2)
-
-    for value in textemail:
-        value.send_keys(data[count])
-        count += 1
-        time.sleep(2)
-
-    for value in textage:
-        value.send_keys(data[count])
-        count += 1
-        time.sleep(2)
-
-    for value in textsalary:
-        value.send_keys(data[count])
-        count += 1
-        time.sleep(2)
-
-    for value in textdepartment:
-        value.send_keys(data[count])
-        count += 1
-        time.sleep(2)
+    driver.find_element_by_id('firstName').send_keys(data['firstName'])
+    driver.find_element_by_id('lastName').send_keys(data['lastName'])
+    driver.find_element_by_id('userEmail').send_keys(data['userEmail'])
+    driver.find_element_by_id('age').send_keys(data['age'])
+    driver.find_element_by_id('salary').send_keys(data['salary'])
+    driver.find_element_by_id('department').send_keys(data['department'])
 
     submit = driver.find_element_by_id('submit')
     submit.click()
-    
+
+driver.close()
